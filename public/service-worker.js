3cdd976aa74cee8908fb88e702b28525356cdf29
JudyMotha <judyxaviermotha@gmail.com>
2021-09-17 14:54:46 -0400

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,67 +13,67 @@ const FILES_TO_CACHE = [
   "/service-worker.js"
 
 ];
-
-self.addEventListener('install', function(evt) {
-    evt.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log('Your files were pre-cached successfully!');
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
-    self.skipWaiting();
-})
-
-
-self.addEventListener('activate', function(evt) {
-    evt.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log('Removing old cache data', key);
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
-    );
-    self.clients.claim();
-})
-
-
-self.addEventListener('fetch', function(evt) {
-    if (evt.request.url.includes('/api/transaction')) {
-        evt.respondWith(
-          caches
-            .open(DATA_CACHE_NAME)
-            .then(cache => {
-              return fetch(evt.request)
-                .then(response => {
-                  if (response.status === 200) {
-                    cache.put(evt.request.url, response.clone());
-                  }
-    
-                  return response;
-                })
-                .catch(err => {
-                  return cache.match(evt.request);
-                });
-            })
-            .catch(err => console.log(err))
-        );
-    
-        return;
-      }
-      evt.respondWith(
-        fetch(evt.request).catch(function() {
-          return caches.match(evt.request).then(function(response) {
-            if (response) {
-              return response;
-            } else if (evt.request.headers.get('accept').includes('text/html')) {
-              return caches.match('/');
-            }
-          });
-        })
-      );
+//INSTALL ACTIVATE FETCH CYCLES
+self.addEventListener(`install`, 
+                      (evt) => {
+  evt.waitUntil(
+    caches
+      .open(PRE_CACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
 });
+
+self.addEventListener(`activate`,
+                      (evt) => {
+  const currentCaches = [PRE_CACHE, RUNTIME_CACHE];
+  evt.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener(`fetch`,
+                      (evt) => {
+  if (evt.request.method !== `GET` || !e.request.url.startsWith(self.location.origin)) {
+    evt.respondWith(fetch(evt.request));
+    return;
+  }
+
+  if (evt.request.url.includes(`/api/transaction`)) {
+    evt.respondWith(caches.open(RUNTIME_CACHE)
+      .then(cache => fetch(e.request)
+        .then(response => {cache.put(evt.request, response.clone());
+          return response;
+        })
+        .catch(() => caches.match(evt.request))
+      )
+    );
+    return;
+  }
+
+  evt.respondWith(caches.match(evt.request)
+    .then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return caches
+      .open(RUNTIME_CACHE)
+      .then(cache => fetch(e.request)
+      .then(response => cache.put(evt.request, response.clone())
+        .then(() => response)
+      ));
+  }));
+});
+
