5ea163a034436ede36df3cd7099f17b570e42527
JudyMotha <judyxaviermotha@gmail.com>
2021-09-17 15:10:23 -0400

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,3 +13,70 @@ const FILES_TO_CACHE = [
   "/service-worker.js"
 
 ];
+//INSTALL ACTIVATE FETCH CYCLES
+self.addEventListener(`install`, 
+                      (evt) => {
+  evt.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
+});
+
+
+//ACT
+self.addEventListener(`activate`,
+                      (evt) => {
+  const currentCaches = [CACHE_NAME,DATA_CACHE_NAME];
+  evt.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+//FETCH
+self.addEventListener(`fetch`,
+                      (evt) => {
+  if (evt.request.method !== `GET` || !evt.request.url.startsWith(self.location.origin)) {
+    evt.respondWith(fetch(evt.request));
+    return;
+  }
+
+  if (evt.request.url.includes(`/api/transaction`)) {
+    evt.respondWith(caches.open(DATA_CACHE_NAME)
+      .then(cache => fetch(evt.request)
+        .then(response => {cache.put(evt.request, response.clone());
+          return response;
+        })
+        .catch(() => caches.match(evt.request))
+      )
+    );
+    return;
+  }
+
+  evt.respondWith(caches.match(evt.request)
+    .then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return caches
+      .open(DATA_CACHE_NAME)
+      .then(cache => fetch(evt.request)
+      .then(response => cache.put(evt.request, response.clone())
+        .then(() => response)
+      ));
+  }));
+});
+
